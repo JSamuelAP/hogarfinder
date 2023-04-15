e10965d9cecd42a5adc85a267e8df74b73824558
JSamel <sp4619168@gmail.com>
2023-04-14 18:36:57 -0600

    Formulario para reportar empresas

--- src/controllers/negocio.controller.js
+++ src/controllers/negocio.controller.js
@@ -8,3 +8,8 @@ export const getNegocio = async (req, res) => {
 export const renderEditarNegocio = async (req, res) => {
 	res.render("editar-perfil-negocio");
 };
+
+export const renderReportarNegocio = async (req, res) => {
+	const id = req.params.id;
+	res.render("reportar-negocio", {id});
+}
--- src/routes/negocio.routes.js
+++ src/routes/negocio.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
 	getNegocio,
 	renderEditarNegocio,
+	renderReportarNegocio
 } from "../controllers/negocio.controller";
 
 const router = Router();
 
 router.get("/perfil-negocio/:id", getNegocio);
+router.get("/perfil-negocio/:id/reportar-negocio", renderReportarNegocio);
 router.get("/editar-perfil-negocio", renderEditarNegocio);
 
 export default router;
