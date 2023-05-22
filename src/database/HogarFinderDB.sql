948d504a3896ff953035a3a7452a53a9f8c944fa
JSamel <sp4619168@gmail.com>
2023-05-21 23:23:21 -0600

    Poblar base de datos con ejemplos finales
    Tambien se agregaron imagenes y archivos pdf para los usuarios.

--- src/database/HogarFinderDB.sql
+++ src/database/HogarFinderDB.sql
@@ -1,96 +1,91 @@
-create database HogarFinder
-use HogarFinder
+CREATE DATABASE HogarFinder
+USE HogarFinder
+
+CREATE TABLE Cliente (
+	ID_Cliente int identity(1,1), 
+	Nombre char(30) NOT NULL,
+	Apellido char(30) NOT NULL,
+	Correo_Electronico char(40) UNIQUE NOT NULL,
+	Contraseña char(30) NOT NULL,
+	foto char(25),
+	primary key ( ID_Cliente )
+);
 
 
-create table Cliente
-(ID_Cliente int identity(1,1), 
-Nombre char(30) not null,
-Apellido char(30) not null,
-Correo_Electronico char(40) not null,
-Contraseña char(30) not null,
-foto varbinary,
-primary key ( ID_Cliente ))
-
-ALTER TABLE Cliente ADD CONSTRAINT UQ_Correo UNIQUE (Correo_Electronico);
-ALTER TABLE Cliente ALTER COLUMN foto char(25);
-
-create table Negocio
-(ID_Negocio int identity(1,1),
-Nombre_negocio char (40) not null,
-Correo_Electronico char(40)not null,
-Contraseña char (40) not null,
-Telefono_Negocio varchar(10) not null,
-Domicilio char (40) not null,
-Tipo_Negocio char (40),
-Tipo_servicio char (40),
-foto varbinary,
-primary key (ID_Negocio))
-
-ALTER TABLE Negocio ADD CONSTRAINT UQ_CorreoNegocio UNIQUE (Correo_Electronico);
-ALTER TABLE Negocio ALTER COLUMN foto char(25);
-
-create table Administrador
-(ID_Administrador int identity(1,1),
-Nombre char (30) not null,
-Apellido char (30) not null,
-Correo_Electronico char(40) not null,
-Contraseña char (30) not null,
-primary key (ID_Administrador))
-
-ALTER TABLE Administrador ADD CONSTRAINT UQ_CorreoAdmin UNIQUE (Correo_Electronico);
-
-create table Calificacion
-(Id_Calificacion int identity(1,1),
-ID_Negocio int not null,
-ID_Cliente int not null,
-Puntaje int,
-Comentario varchar(max),
-primary key (id_calificacion),
-Foreign key (ID_Negocio) references Negocio (ID_Negocio),
-Foreign Key (ID_Cliente) references Cliente (ID_Cliente))
+CREATE TABLE Negocio (
+	ID_Negocio int identity(1,1),
+	Nombre_negocio char (40) NOT NULL,
+	Correo_Electronico char(40) UNIQUE NOT NULL,
+	Contraseña char (40) NOT NULL,
+	Telefono_Negocio varchar(10) NOT NULL,
+	Domicilio char (40) NOT NULL,
+	Tipo_Negocio char (40),
+	Tipo_servicio char (40),
+	foto char(25),
+	primary key (ID_Negocio)
+);
 
 
-create table Negocios_Favoritos
-(ID_Cliente int not null,
-ID_Negocio int not null,
-Foreign key (ID_Negocio) references Negocio (ID_Negocio),
-Foreign Key (ID_Cliente) references Cliente (ID_Cliente))
-
-ALTER TABLE Negocios_Favoritos ADD CONSTRAINT UQ_Favorito UNIQUE (ID_Cliente, ID_Negocio);
-
-create table Solicitud
-(ID_solicitud int identity(1,1),
-ID_Negocio int not null,
-Comprobante_domicilio varbinary,
-INE varbinary,
-RFC varbinary,
-Estado char(30),
-primary key (ID_solicitud),
-Foreign key (ID_Negocio) references Negocio (ID_Negocio))
-
-ALTER TABLE Solicitud ADD CONSTRAINT UQ_Solicitud UNIQUE (ID_Negocio);
-ALTER TABLE Solicitud ALTER COLUMN Comprobante_domicilio char(25);
-ALTER TABLE Solicitud ALTER COLUMN INE char(25);
-ALTER TABLE Solicitud ALTER COLUMN RFC char(25);
-
-create table publicacion
-(ID_Post int identity(1,1),
-ID_Negocio int not null,
-Titulo char(50),
-Descripcion char (200),
-foto varbinary,
-Fecha_creacion date,
-precio decimal,
-primary key (ID_Post),
-Foreign key (ID_Negocio) references Negocio (ID_Negocio))
-
-ALTER TABLE publicacion ALTER COLUMN foto char(25);
+CREATE TABLE Administrador (
+	ID_Administrador int identity(1,1),
+	Nombre char (30) NOT NULL,
+	Apellido char (30) NOT NULL,
+	Correo_Electronico char(40) UNIQUE NOT NULL,
+	Contraseña char (30) NOT NULL,
+	primary key (ID_Administrador)
+);
 
 
-create table reporte (
-	ID_Cliente int not null,
-	ID_Negocio int not null,
-	problema varchar(200) not null,
+CREATE TABLE Calificacion (
+	Id_Calificacion int identity(1,1),
+	ID_Negocio int NOT NULL,
+	ID_Cliente int NOT NULL,
+	Puntaje int,
+	Comentario varchar(max),
+	primary key (id_calificacion),
 	Foreign key (ID_Negocio) references Negocio (ID_Negocio),
 	Foreign Key (ID_Cliente) references Cliente (ID_Cliente)
-)
+);
+
+
+CREATE TABLE Negocios_Favoritos (
+	ID_Cliente int NOT NULL,
+	ID_Negocio int NOT NULL,
+	Foreign key (ID_Negocio) references Negocio (ID_Negocio),
+	Foreign Key (ID_Cliente) references Cliente (ID_Cliente),
+	CONSTRAINT UQ_Favorito UNIQUE (ID_Cliente, ID_Negocio)
+);
+
+
+CREATE TABLE Solicitud (
+	ID_solicitud int identity(1,1),
+	ID_Negocio int UNIQUE NOT NULL,
+	Comprobante_domicilio char(25),
+	INE char(25),
+	RFC char(25),
+	Estado char(30),
+	primary key (ID_solicitud),
+	Foreign key (ID_Negocio) references Negocio (ID_Negocio)
+);
+
+
+CREATE TABLE publicacion (
+	ID_Post int identity(1,1),
+	ID_Negocio int NOT NULL,
+	Titulo char(50),
+	Descripcion char (200),
+	foto char(25),
+	Fecha_creacion date,
+	precio decimal,
+	primary key (ID_Post),
+	Foreign key (ID_Negocio) references Negocio (ID_Negocio)
+);
+
+
+CREATE TABLE reporte (
+	ID_Cliente int NOT NULL,
+	ID_Negocio int NOT NULL,
+	problema varchar(200) NOT NULL,
+	Foreign key (ID_Negocio) references Negocio (ID_Negocio),
+	Foreign Key (ID_Cliente) references Cliente (ID_Cliente)
+);
--- src/database/tuplasHF.sql
+++ src/database/tuplasHF.sql
@@ -1,60 +1,75 @@
-insert into Cliente(Nombre, Apellido, Correo_Electronico, Contraseña)
-	values ('Santiago','Hernández','santiago.hernandez@gmail.com','Algo_1234'),
+INSERT INTO Cliente (Nombre, Apellido, Correo_Electronico, Contraseña)
+	VALUES ('Santiago','Hernández','santiago.hernandez@gmail.com','Algo_1234'),
 		   ('Ana', 'García', 'ana.garcia@gmail.com', 'Algo_1234'),
 		   ('Marcos', 'Ríos', 'marcos.rios@gmail.com', 'Algo_1234'),
 		   ('Gabriela', 'Fernández', 'gabriela.fdez@gmail.com','Algo_1234'),
 		   ('Carlos', 'Ramirez', 'carlos.ramirez@gmail.com', 'Algo_1234');
 
 
-insert into Negocio (Nombre_negocio,Correo_Electronico,Contraseña, Telefono_Negocio, Domicilio, Tipo_servicio)
-	values ('LimpiezaExpressHogar','limpiezaexHogar@gmail.com', 'Negocio_246', '4772930482', 'AV. J ORTIZ DE DEOMINGUEZ NO. 518', 'LIMPIEZA'),
-		   ('Jardinez en orden', 'jardinezenorden@gmail.com', 'Negocio_246', '4772394819', 'HERMANOS ESCOBAR NO. 3213', 'JARDINERIA'),
-		   ('ReparaYAhogar', 'repara_yahogar@gmail.com', 'Negocio_246', '4772939408', 'AV. DE LAS FUETES NO. 5500', 'REPARACIONES'),
-		   ('ServiHogar', 'servihogar@gmail.com', 'Negocio_246', '4772948294', '24 A SUR 3539, SANTA MONICA', 'SERVICIOS DOMESTICOS'),
-		   ('Jardineria ProHogar', 'jardineria_prohogar@gmail.com','Negocio_246', '4778029482', 'LAZARO CARDENAS NO. 2471', 'JARDINERIA');
+INSERT INTO Negocio
+	(Nombre_negocio, Correo_Electronico, Contraseña, Telefono_Negocio, Domicilio, Tipo_Negocio, Tipo_servicio)
+	VALUES
+		('Limpieza Express Hogar','limpiezaexpress@gmail.com', 'Negocio_246', '4772930482', 'AV. J ORTIZ DE DEOMINGUEZ NO. 518', 'Empresa', 'Limpieza'),
+		('Jardines en orden', 'jardinezenorden@gmail.com', 'Negocio_246', '4772394819', 'HERMANOS ESCOBAR NO. 3213', 'Empresa', 'Jardineria'),
+		('Repara y Ahorra', 'reparayahorra@gmail.com', 'Negocio_246', '4772939408', 'AV. DE LAS FUENTES NO. 5500', 'Trabajador autonómo', 'Reparacion'),
+		('Super Bros Plomeria', 'mario@gmail.com', 'Negocio_246', '4772948294', '24 A SUR 3539, Brooklyn', 'Empresa', 'Plomeria'),
+		('Juan Electrico', 'juan@gmail.com','Negocio_246', '4778029482', 'LAZARO CARDENAS NO. 2471', 'Trabajador autonómo', 'Electricidad'),
+		('Gepetto', 'gepetto@gmail.com','Negocio_246', '4778029482', 'LAZARO CARDENAS NO. 2471, LAZIO', 'Trabajador autonómo', 'Carpinteria'),
+		('Pinturas Leonardo Da Vinci', 'leonardopinturas@gmail.com','Negocio_246', '4778029482', 'LA TOSCANA NO. 1941, Florencia', 'Empresa', 'Pintura'),
+		('Servicio de niñera Vicky', 'vicky@gmail.com','Negocio_246', '4778029482', 'Calle Magica NO. 12, Dimmsdale', 'Trabajador autonómo', 'Niñeras');
 
-UPDATE Negocio SET Tipo_Negocio = 'Empresa' WHERE ID_Negocio = 1;
-UPDATE Negocio SET Tipo_Negocio = 'Trabajador autónomo' WHERE ID_Negocio = 2;
-UPDATE Negocio SET Tipo_Negocio = 'Empresa' WHERE ID_Negocio = 3;
-UPDATE Negocio SET Tipo_Negocio = 'Trabajador autónomo' WHERE ID_Negocio = 4;
-UPDATE Negocio SET Tipo_Negocio = 'Empresa' WHERE ID_Negocio = 5;
 
-
-insert into Administrador
-	values ('Jose Samuel', 'Aldana', 'josesamuel@gmail.com', 'Admin_1234'),
+INSERT INTO Administrador
+	VALUES ('Samuel', 'Aldana', 'josesamuel@gmail.com', 'Admin_1234'),
 		   ('Jessica', 'Jantes', 'jesicajantes@gmail.com', 'Admin_123'),
 		   ('Mauricio', 'Vela', 'maurixiovela@gmail.com', 'Admin_123'),
-		   ('Juan Carlos', 'Carrera', 'juancarlos@gmail.com', 'Admin_123'),
-		   ('Brandon', 'Aguiñaga', 'brandonaguiñaga@gmail.com', 'Admin_123');
+		   ('Carlos', 'Carrera', 'juancarlos@gmail.com', 'Admin_123');
 
 
-insert into Calificacion
-	values (1, 1, 4, 'Muy buen servicio'),
-		   (2,2,5, 'Cuidan bien las plantas'),
-		   (3,3,3, 'el resultado fue el esperado'),
-		   (4,4, 4, 'me agrado el servicio ofrecido'),
-		   (5,5, 5, 'Lo volveria a contratar');
+INSERT INTO Calificacion
+	VALUES (1, 1, 4, 'Muy buen servicio'),
+		   (2, 1, 5, 'Cuidan bien las plantas'),
+		   (3, 2, 1, 'Cobran demasiado y no reparan bien'),
+		   (4, 2, 2, 'Dejaron un desastre en mi baño'),
+		   (5, 3, 3, 'Cobra un poco caro pero cumple con su trabajo'),
+		   (6, 4, 4, 'Hace trabajos hermosos, con mucho detalle y dedicación'),
+		   (7, 4, 5, 'Trabaja rapido y muy bien, me agrada'),
+		   (8, 5, 5, 'Muy confiable, trata bien a los niños');
 
 
-insert into Negocios_Favoritos
-	values (1, 3), (2, 4), (3, 1), (4, 2), (5, 5), (1, 1), (2, 5), (3, 3), (4, 4), (5, 2);
+INSERT INTO Negocios_Favoritos
+	VALUES (1, 1), (1, 2), (2, 4), (2, 5), (3, 1), (3, 7), (4, 6), (4, 7), (5, 8), (5, 2);
 
 
-insert into Solicitud (ID_Negocio, Estado)
-	values (1, 'En revisión'), (2, 'Aceptada'), (3, 'Rechazada'), (4, 'En revisión'), (5, 'En revisión');
+INSERT INTO Solicitud (ID_Negocio, Comprobante_domicilio, INE, RFC, Estado)
+	VALUES (1, 'comprobante.pdf', 'ine.pdf', 'rfc.pdf', 'En revisión'),
+		   (2, 'comprobante.pdf', 'ine.pdf', 'rfc.pdf', 'En revisión'),
+		   (3, 'comprobante.pdf', 'ine.pdf', 'rfc.pdf', 'En revisión'),
+		   (4, 'comprobante.pdf', 'ine.pdf', 'rfc.pdf', 'En revisión'),
+		   (5, 'comprobante.pdf', 'ine.pdf', 'rfc.pdf', 'En revisión'),
+		   (6, 'comprobante.pdf', 'ine.pdf', 'rfc.pdf', 'En revisión'),
+		   (7, 'comprobante.pdf', 'ine.pdf', 'rfc.pdf', 'En revisión'),
+		   (8, 'comprobante.pdf', 'ine.pdf', 'rfc.pdf', 'En revisión');
 
 
-INSERT INTO publicacion (ID_Negocio, Titulo, Descripcion, Fecha_creacion, precio)
-	VALUES (1, 'Limpieza Profunda de Hogar', 'Ofrecemos servicios de limpieza profesional para el hogar. Nos aseguramos de que cada rincón de tu casa quede impecable', '2023-02-11', 1200.00),
-		   (2, 'Servicios de Jardinería', 'Realizamos servicios de jardinería profesional para mantener su jardín hermoso y cuidado. Ofrecemos mantenimiento regular o servicios de diseño y paisajismo', '2022-06-11', 2000.00),
-		   (3, 'Reparación de Electrodomésticos', 'Somos especialistas en la reparación de electrodomésticos, como lavadoras, secadoras, refrigeradores, etc. Ofrecemos precios justos y servicio de calidad', '2022-02-10', 1500.00),
-		   (4, 'Servicios Domésticos', 'Ofrecemos una amplia gama de servicios domésticos, desde limpieza hasta cuidado de niños y ancianos. Nos aseguramos de que tu hogar esté en buenas manos', '2020-01-24', 1800.00),
-		   (5, 'Mantenimiento de Jardines', 'Ofrecemos servicios de mantenimiento de jardines y patios para mantener su espacio al aire libre limpio y bien cuidado', '2023-04-16', 1500.00);
+INSERT INTO publicacion (ID_Negocio, Titulo, Descripcion, Fecha_creacion, precio, foto)
+	VALUES
+		(1, 'Limpieza Profunda de Hogar', 'Ofrecemos servicios de limpieza profesional para el hogar. Nos aseguramos de que cada rincón de tu casa quede impecable', '2023-02-11', 1200.00, 'productos-limpieza.jpg'),
+		(2, 'Servicios de Jardinería', 'Realizamos servicios de jardinería profesional para mantener su jardín hermoso y cuidado.', '2022-06-11', 2000.00, 'jardin.jpg'),
+ 	    (3, 'Reparación de Electrodomésticos', 'Somos especialistas en la reparación de electrodomésticos, como lavadoras, secadoras, refrigeradores, etc.', '2022-02-10', 1500.00, 'reparaciones.jpg'),
+ 	    (4, 'Reparación de tuberías', '¿Tiene fugas de agua en su baño o cocina? Los super hermanos Mario y Luiggi a su disposición para tratar cualquier problema de plomería.', '2020-01-24', 1800.00, 'plomeria.jpg'),
+		(5, 'Electrico', 'más de 10 años de experiencia, doy solución a problemas de electricidad de su hogar siguiendo protocolos de seguridad en tiempo record.', '2022-05-11', 850.00, 'electricidad.jpg'),
+		(6, 'Muebles de madera', 'Buenos días comunidad, ofrezco mis servicios de carpinteriá, realizo sillas, mesas y cualquier acabado de madera, garantizo alta calidad.', '2023-04-16', 900.00, 'carpinteria.jpg'),	
+		(7, 'Pintura y decoración de fachadas', 'Pintamos todo tipo de fachadas con materiales de calidad, mi equipo se encargara de dejar su hogar bonito y duradero.', '2021-03-15', 500.00, 'pintura.jpg'),
+		(8, 'Servicio de niñera', 'Cuido niños por las tardes, se cocinar y doy clases de matematicas e inglés para sus pequeños.', '2023-05-04', 300.00, 'niñera.jpg');
 
 INSERT INTO reporte
-	VALUES (1, 2, 'La empresa no llego a mi domicilio y me exigio que aun así les hiciera el deposito'),
-		   (5, 1, 'Robaron varias flores de mi jardín, segun ellos fue porque ya estaban marchitadas, y cuando se las pedí no me las devolvieron.'),
-		   (3, 3, 'Ya va dos veces que mando a reparar mi tele y lavadora y dicen que no tiene solución, contrate a otro tecnico y me los reparo sin problemas');
+	VALUES
+		(1, 2, 'La empresa no llego a mi domicilio y me exigio que aun así les hiciera el deposito'),
+		(2, 2, 'Robaron varias flores de mi jardín, segun ellos fue porque ya estaban marchitadas, y cuando se las pedí no me las devolvieron.'),
+		(3, 3, 'Ya va dos veces que mando a reparar mi tele y lavadora y dicen que no tiene solución, contrate a otro tecnico y me los reparo sin problemas'),
+		(4, 1, 'Utilizaron productos dañinos para mi piso de madera y no quieren pagar su tratamiento'),
+		(1, 8, 'No hace bien su trabajo, no cuida a los niños y solo ve la tele y agarra comida de mi cocina');
 
 SELECT * FROM Cliente;
 SELECT * FROM Negocio;
@@ -65,6 +80,7 @@ SELECT * FROM Solicitud;
 SELECT * FROM publicacion;
 SELECT * FROM reporte;
 
+-- Pruebas revisadas en clase, no ejecutar
 INSERT INTO Cliente (Nombre, Apellido, Correo_Electronico, Contraseña)
 OUTPUT INSERTED.ID_Cliente
 VALUES ('Jose', 'Perez', 'jose1@gmail.com', 'patito26')
